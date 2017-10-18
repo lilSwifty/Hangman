feb416622d3f520b24a1d2e5b4c86ebb13506f2c
Manolo <manisedighi@icloud.com>
2017-10-18 11:06:21 +0200

    Innan ändring av toolbar

--- app/src/main/java/com/example/manisedighi/hangman/MainActivity.java
+++ app/src/main/java/com/example/manisedighi/hangman/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.manisedighi.hangman;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAboutClicked(View view){
+        Intent intent = new Intent(this, AboutActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void onPlayClicked(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+
+        startActivity(intent);
     }
 }
