da48eb2572d99c426869a33ca2dde148361e0f00
Manolo <manisedighi@icloud.com>
2017-10-23 09:58:53 +0200

    about-funktionen klar

--- app/src/main/java/com/example/manisedighi/hangman/MainActivity.java
+++ app/src/main/java/com/example/manisedighi/hangman/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.manisedighi.hangman;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,9 +23,26 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.about_logo):
+                onAboutClicked(findViewById(R.id.about_logo));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void onPlayClicked(View view){
         Intent intent = new Intent(this, GameActivity.class);
 
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
